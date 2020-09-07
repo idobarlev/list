2e83339dcc987b5375f509176c7a2710ecab59f2
Ido Bar Lev <idobarlev2@gmail.com>
2020-09-07 17:16:06 +0300

    Register and Login
    ⭐Basic register and login.
    ❗ Sign out

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,12 +1,66 @@
 <template>
   <div class="login">
-    <h1>login</h1>
+    <h1>Login</h1>
+    <v-form v-model="valid" @submit.prevent="loginWithEmail">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="4">
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+            <v-text-field
+              v-model="password"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="showPassword ? 'text' : 'password'"
+              name="input-10-1"
+              label="Password"
+              hint="At least 8 characters"
+              counter
+              @click:append="showPassword = !showPassword"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-btn type="submit">Login</v-btn>
+      </v-container>
+    </v-form>
+    <v-btn>Register now 😁</v-btn>
   </div>
 </template>
 
 <script>
-export default {};
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+export default {
+  data: () => ({
+    valid: false,
+    password: "",
+    showPassword: false,
+    rules: {
+      required: (value) => !!value || "Required.",
+      min: (v) => v.length >= 8 || "Min 8 characters",
+      emailMatch: () => "The email and password you entered don't match",
+    },
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+  methods: {
+    loginWithEmail() {
+      firebase
+        .auth()
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          console.log("register successfuly! 😁");
+          this.$router.replace({ name: "home" });
+        })
+        .catch((error) => (this.error = error));
+    },
+  },
+};
 </script>
+
 
 <style lang="css" scoped>
 </style>
--- src/views/register.vue
+++ src/views/register.vue
@@ -1,42 +1,62 @@
 <template>
   <div class="register">
-    <h1>register</h1>
-    <v-form v-model="valid">
+    <h1>Register</h1>
+    <v-form v-model="valid" @submit.prevent="registerWithEmail">
       <v-container>
         <v-row>
           <v-col cols="12" md="4">
-            <v-col cols="12" md="4">
-              <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-            </v-col>
+            <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
             <v-text-field
               v-model="password"
-              :rules="nameRules"
-              :counter="10"
+              :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
+              :rules="[rules.required, rules.min]"
+              :type="showPassword ? 'text' : 'password'"
+              name="input-10-1"
               label="Password"
-              required
+              hint="At least 8 characters"
+              counter
+              @click:append="showPassword = !showPassword"
             ></v-text-field>
           </v-col>
         </v-row>
+        <v-btn type="submit">Register</v-btn>
       </v-container>
     </v-form>
   </div>
 </template>
 
 <script>
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
 export default {
   data: () => ({
     valid: false,
     password: "",
-    passwordRules: [
-      (v) => !!v || "Password is required",
-      (v) => v.length >= 6 || "Password must be more than 6 characters",
-    ],
+    showPassword: false,
+    rules: {
+      required: (value) => !!value || "Required.",
+      min: (v) => v.length >= 8 || "Min 8 characters",
+      emailMatch: () => "The email and password you entered don't match",
+    },
     email: "",
     emailRules: [
       (v) => !!v || "E-mail is required",
       (v) => /.+@.+/.test(v) || "E-mail must be valid",
     ],
   }),
+  methods: {
+    registerWithEmail() {
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          console.log("register successfuly! 😁");
+          this.$router.replace({ name: "home" });
+        })
+        .catch((error) => (this.error = error));
+    },
+  },
 };
 </script>
 
