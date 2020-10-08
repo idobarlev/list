0d7cc2661ff7e268659aa1b55de3f896a9959118
Ido Bar Lev <idobarlev2@gmail.com>
2020-10-08 17:41:48 +0300

    Continue create task

--- src/views/createList.vue
+++ src/views/createList.vue
@@ -5,20 +5,29 @@
       <v-container>
         <v-row align="center" justify="center">
           <v-col cols="12" md="4">
-            <v-text-field v-model="listName" label="Event list name" required></v-text-field>
+            <v-text-field
+              v-model="listName"
+              label="Event list name"
+              :rules="listNameRules"
+              required
+            ></v-text-field>
 
             <v-checkbox v-model="isRepeat" :label="`Repeat?`"></v-checkbox>
           </v-col>
         </v-row>
         <div v-show="!isRepeat">
           <v-row align="center" justify="center">
-            <v-col cols="12" md="4">
+            <v-col cols="8" md="4">
               <v-date-picker v-model="date"></v-date-picker>
             </v-col>
           </v-row>
           <v-row align="center" justify="center">
             <v-col cols="12" md="4">
-              <v-text-field v-model="dateText" label="Event date" prepend-icon="mdi-calendar"></v-text-field>
+              <v-text-field
+                v-model="dateText"
+                label="Event date"
+                prepend-icon="mdi-calendar"
+              ></v-text-field>
             </v-col>
           </v-row>
         </div>
@@ -30,23 +39,47 @@
           </v-row>
           <v-row align="center" justify="center">
             <v-col cols="12" md="4">
-              <v-text-field v-model="dateRangeText" label="Date range" prepend-icon="mdi-calendar"></v-text-field>
+              <v-text-field
+                v-model="dateRangeText"
+                label="Date range"
+                prepend-icon="mdi-calendar"
+              ></v-text-field>
             </v-col>
           </v-row>
           <b>Repeat every:</b>
-          <p>{{repeatDisplay()}}</p>
+          <p>{{ repeatDisplay() }}</p>
           <v-row align="center" justify="center">
-            <v-col cols="2" md="1">
-              <v-text-field type="number" v-model="days" label="Days" required></v-text-field>
+            <v-col cols="3" md="1">
+              <v-text-field
+                type="number"
+                v-model="repeat.days"
+                label="Days"
+                required
+              ></v-text-field>
             </v-col>
-            <v-col cols="2" md="1">
-              <v-text-field type="number" v-model="weeks" label="Weeks" required></v-text-field>
+            <v-col cols="3" md="1">
+              <v-text-field
+                type="number"
+                v-model="repeat.weeks"
+                label="Weeks"
+                required
+              ></v-text-field>
             </v-col>
-            <v-col cols="2" md="1">
-              <v-text-field type="number" v-model="months" label="Months" required></v-text-field>
+            <v-col cols="3" md="1">
+              <v-text-field
+                type="number"
+                v-model="repeat.months"
+                label="Months"
+                required
+              ></v-text-field>
             </v-col>
-            <v-col cols="2" md="1">
-              <v-text-field type="number" v-model="years" label="Years" required></v-text-field>
+            <v-col cols="3" md="1">
+              <v-text-field
+                type="number"
+                v-model="repeat.years"
+                label="Years"
+                required
+              ></v-text-field>
             </v-col>
           </v-row>
         </div>
@@ -61,15 +94,18 @@ export default {
   data: () => ({
     listName: "",
     isRepeat: false,
-    days: 0,
-    weeks: 0,
-    months: 0,
-    years: 0,
+    repeat: {
+      days: 0,
+      weeks: 0,
+      months: 0,
+      years: 0,
+    },
     date: new Date().toISOString().substr(0, 10),
     dates: [
       new Date().toISOString().substr(0, 10),
-      new Date().toISOString().substr(0, 10)
-    ]
+      new Date().toISOString().substr(0, 10),
+    ],
+    listNameRules: [(v) => !!v || "List name is rqeuired"],
   }),
   computed: {
     dateRangeText() {
@@ -97,56 +133,83 @@ export default {
       }
 
       return `${day}/${month}/${year}`;
-    }
+    },
   },
   methods: {
     createList() {
       console.log(`create some list`);
+      let dates = "";
+
+      if (this.isRepeat) {
+        dates = this.dates;
+      } else {
+        dates = this.date;
+      }
+
+      let listData = {
+        listName: this.listName,
+        isRepeat: this.isRepeat,
+        dates: dates,
+        repeat: this.repeat,
+      };
+
+      console.log(listData);
     },
     repeatDisplay() {
       let repeatStr = "";
 
-      if (this.days > 0) {
-        repeatStr += this.days + " Day";
+      if (this.repeat.days > 0) {
+        repeatStr += this.repeat.days + " Day";
 
-        if (this.days > 1) {
+        if (this.repeat.days > 1) {
           repeatStr += "s";
         }
 
         repeatStr += " ";
       }
-      if (this.weeks > 0) {
-        if (this.months <= 0 && this.years <= 0 && this.days > 0) {
+      if (this.repeat.weeks > 0) {
+        if (
+          this.repeat.months <= 0 &&
+          this.repeat.years <= 0 &&
+          this.repeat.days > 0
+        ) {
           repeatStr += "and ";
         }
 
-        repeatStr += this.weeks + " Week";
+        repeatStr += this.repeat.weeks + " Week";
 
-        if (this.weeks > 1) {
+        if (this.repeat.weeks > 1) {
           repeatStr += "s";
         }
 
         repeatStr += " ";
       }
-      if (this.months > 0) {
-        if (this.years <= 0 && (this.days > 0 || this.weeks > 0)) {
+      if (this.repeat.months > 0) {
+        if (
+          this.repeat.years <= 0 &&
+          (this.repeat.days > 0 || this.repeat.weeks > 0)
+        ) {
           repeatStr += "and ";
         }
-        repeatStr += this.months + " Month";
+        repeatStr += this.repeat.months + " Month";
 
-        if (this.months > 1) {
+        if (this.repeat.months > 1) {
           repeatStr += "s";
         }
 
         repeatStr += " ";
       }
-      if (this.years > 0) {
-        if (this.days > 0 || this.weeks > 0 || this.months > 0) {
+      if (this.repeat.years > 0) {
+        if (
+          this.repeat.days > 0 ||
+          this.repeat.weeks > 0 ||
+          this.repeat.months > 0
+        ) {
           repeatStr += "and ";
         }
-        repeatStr += this.years + " Year";
+        repeatStr += this.repeat.years + " Year";
 
-        if (this.years > 1) {
+        if (this.repeat.years > 1) {
           repeatStr += "s";
         }
 
@@ -154,8 +217,8 @@ export default {
       }
 
       return repeatStr;
-    }
-  }
+    },
+  },
 };
 </script>
 
